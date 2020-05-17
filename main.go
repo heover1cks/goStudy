9e11b575f970a6c925e563d051ab20535577a813
heover1cks <heover1cks@gmail.com>
2020-05-17 13:23:26 +0900

    test

--- main.go
+++ main.go
@@ -3,7 +3,25 @@ package main
 import (
 	"fmt"
 	someFunc "goStudy/importTest"
+	"strings"
 )
+
+func multiply(a int, b int) int {
+	return a*b
+}
+
+func multiply2(a,b int) int { //input, return type 명시 필요
+	return a*b
+}
+
+//Multiple Return value -> Go에만 있음
+func lenAndUpper(name string)(int, string){
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) { //...
+	fmt.Println(words)
+}
 
 func main() {
 	fmt.Println("Hello World")
@@ -13,7 +31,7 @@ func main() {
 
 	const name string = "Carlos Sainz"
 	//name = "Charles Leclerc" 못바꿈
-	fmt.Print(name)
+	fmt.Println(name)
 
 	// name3 := "something -> var name3 string = "something"
 	var name2 string = "Sainz"
@@ -21,4 +39,13 @@ func main() {
 
 	fmt.Println(name2)
 	fmt.Println(name3)
+
+	//다중 리턴 펑션
+	totalLength, upperName := lenAndUpper("Ricciardo")
+	fmt.Println(totalLength,upperName)
+	totalLength2,_ := lenAndUpper("Hulkenberg")
+	fmt.Println(totalLength2)
+
+	//repeatMe -> array type return
+	repeatMe("Carlos", "Sainz","Ricciardo","Leclerc")
 }
