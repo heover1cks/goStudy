03bde0f7591c8acc5b48e4b6954c32f010198506
heover1cks <heover1cks@gmail.com>
2020-05-17 13:13:48 +0900

    test

---
+++ importTest/someFunc.go
@@ -0,0 +1,10 @@
+package someFunc
+
+import "fmt"
+
+func sayBye() {
+	fmt.Println("Bye")
+}
+func SayHello(){
+	fmt.Println("Hell_O")
+}
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	someFunc "goStudy/importTest"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	someFunc.SayHello()
+
+	//const: 상수 var: 변수 let:?
+
+	const name string = "Carlos Sainz"
+	//name = "Charles Leclerc" 못바꿈
+	fmt.Print(name)
+
+	// name3 := "something -> var name3 string = "something"
+	var name2 string = "Sainz"
+	name3:= "Carlos"
+
+	fmt.Println(name2)
+	fmt.Println(name3)
 }
